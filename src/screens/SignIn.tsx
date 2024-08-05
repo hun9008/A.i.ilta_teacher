acabe530ba4500101f43639d6730910cf0b7b759
jerry <jerrylee3305@gmail.com>
2024-08-05 16:14:45 +0900

    Fix : Connect Login page server and front

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,65 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../../App';
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Alert,
+} from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../App';
 
-type Props = NativeStackScreenProps<RootStackParamList, 'SignIn'> & { setIsSignedIn: (value: boolean) => void , isSignedIn: boolean};
-
-const SignIn: React.FC<Props> = ({ navigation, setIsSignedIn, isSignedIn }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleSignIn = async () => {
-        /*
-        try {
-            const response = await fetch('https://example.com/api/signin', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, password }),
-            });
-            const result = await response.json();
-    
-            if (response.ok) {
-            setIsSignedIn(true);
-            } else {
-            Alert.alert('Error', result.message || '로그인 실패');
-            }
-        } catch (error) {
-            Alert.alert('Error', '네트워크 오류');
-        }*/
-        setIsSignedIn(true); //백엔드 연결되면 지워야함
-    }
-
-    useEffect(() => {
-        if (isSignedIn) {
-          navigation.replace('MainPage');
-        }
-      }, [isSignedIn, navigation]);
-
-    return (
-        <SafeAreaView>
-        <View>
-            <Text>이메일을 입력하세요</Text>
-            <TextInput
-                placeholder="email"
-                value={email}
-                onChangeText={setEmail}
-            />
-            <Text>비밀번호를 입력하세요</Text>
-            <TextInput
-                placeholder="password"
-                value={password}
-                onChangeText={setPassword}
-            />
-
-            <Button title="로그인 하기" onPress={handleSignIn} />
-            <Button title="비밀번호 찾기" />
-        </View>
-        </SafeAreaView>
-    );
+type Props = NativeStackScreenProps<RootStackParamList, 'SignIn'> & {
+  setIsSignedIn: (value: boolean) => void;
+  isSignedIn: boolean;
 };
 
+const SignIn: React.FC<Props> = ({navigation, setIsSignedIn, isSignedIn}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignIn = async () => {
+    try {
+      const response = await fetch('http://52.141.30.206:8000/Login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({email, password}),
+      });
+      const result = await response.json();
+
+      if (response.ok) {
+        setIsSignedIn(true);
+      } else {
+        Alert.alert('Error', result.message || '로그인 실패');
+      }
+    } catch (error) {
+      Alert.alert('Error', '네트워크 오류');
+    }
+    // setIsSignedIn(true); //백엔드 연결되면 지워야함
+  };
+
+  useEffect(() => {
+    if (isSignedIn) {
+      navigation.replace('MainPage');
+    }
+  }, [isSignedIn, navigation]);
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>이메일을 입력하세요</Text>
+        <TextInput placeholder="email" value={email} onChangeText={setEmail} />
+        <Text>비밀번호를 입력하세요</Text>
+        <TextInput
+          placeholder="password"
+          value={password}
+          onChangeText={setPassword}
+        />
+
+        <Button title="로그인 하기" onPress={handleSignIn} />
+        <Button title="비밀번호 찾기" />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default SignIn;
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,83 +1,88 @@
 import React, {useState, useEffect} from 'react';
-import { SafeAreaView, StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../../App';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Alert,
+} from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../App';
 
-type Props = NativeStackScreenProps<RootStackParamList, 'SignUp'> & { setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>, isSignedIn: boolean };
-
-const SignUp: React.FC<Props> = ({ navigation, setIsSignedIn, isSignedIn }) => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordCheck, setPasswordCheck] = useState('');
-
-    const handleSignUp = async () => {
-        /*if (password !== passwordCheck) {
-          Alert.alert('Error', '비밀번호가 일치하지 않습니다');
-          return;
-        }
-
-        try {
-          const response = await fetch('https://example.com/api/signup', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ name, email, password }),
-          });
-          const result = await response.json();
-    
-          if (response.ok) {
-            setIsSignedIn(true);
-          } else {
-            Alert.alert('Error', result.message || '회원가입 실패');
-          }
-        } catch (error) {
-          Alert.alert('Error', '네트워크 오류');
-        }*/
-        setIsSignedIn(true); //백엔드 연결되면 지워야함
-    };
-
-    useEffect(() => {
-        if (isSignedIn) {
-          navigation.replace('MainPage');
-        }
-      }, [isSignedIn, navigation]);
-
-    return (
-        <SafeAreaView>
-        <View>
-            <Text>이름을 입력하세요</Text>
-            <TextInput
-                placeholder="Name"
-                value={name}
-                onChangeText={setName}
-            />
-            <Text>이메일을 입력하세요</Text>
-            <TextInput
-                placeholder="email"
-                value={email}
-                onChangeText={setEmail}
-            />
-            <Text>비밀번호를 입력하세요</Text>
-            <TextInput
-                placeholder="password"
-                value={password}
-                onChangeText={setPassword}
-            />
-            <Text>비밀번호를 한번 더 입력하세요</Text>
-            <TextInput
-                placeholder="password"
-                value={passwordCheck}
-                onChangeText={setPasswordCheck}
-            />
-
-            <Button title="회원 가입하기" onPress={handleSignUp} />
-            <Button title="이미 계정이 있어요" onPress={() => navigation.navigate('SignIn')} />
-        </View>
-        </SafeAreaView>
-    );
+type Props = NativeStackScreenProps<RootStackParamList, 'SignUp'> & {
+  setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>;
+  isSignedIn: boolean;
 };
 
+const SignUp: React.FC<Props> = ({navigation, setIsSignedIn, isSignedIn}) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+
+  const handleSignUp = async () => {
+    if (password !== passwordCheck) {
+      Alert.alert('Error', '비밀번호가 일치하지 않습니다');
+      return;
+    }
+
+    try {
+      const response = await fetch('http://52.141.30.206:8000/Register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({name, email, password}),
+      });
+      const result = await response.json();
+      // console.log('Response:', result); //로그 찍어보기
+
+      if (response.ok) {
+        setIsSignedIn(true);
+      } else {
+        Alert.alert('Error', result.message || '회원가입 실패');
+      }
+    } catch (error) {
+      Alert.alert('Error', '네트워크 오류');
+    }
+  };
+
+  useEffect(() => {
+    if (isSignedIn) {
+      navigation.replace('MainPage');
+    }
+  }, [isSignedIn, navigation]);
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>이름을 입력하세요</Text>
+        <TextInput placeholder="Name" value={name} onChangeText={setName} />
+        <Text>이메일을 입력하세요</Text>
+        <TextInput placeholder="email" value={email} onChangeText={setEmail} />
+        <Text>비밀번호를 입력하세요</Text>
+        <TextInput
+          placeholder="password"
+          value={password}
+          onChangeText={setPassword}
+        />
+        <Text>비밀번호를 한번 더 입력하세요</Text>
+        <TextInput
+          placeholder="password"
+          value={passwordCheck}
+          onChangeText={setPasswordCheck}
+        />
+
+        <Button title="회원 가입하기" onPress={handleSignUp} />
+        <Button
+          title="이미 계정이 있어요"
+          onPress={() => navigation.navigate('SignIn')}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default SignUp;
