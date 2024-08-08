651edb6b4c361836482c9c81e51fb269cf9b1831
jaeyoungchoi1 <choijy5847@naver.com>
2024-08-08 15:06:47 +0900

    fix : reform camerause page

--- src/screens/CameraUse.tsx
+++ src/screens/CameraUse.tsx
@@ -1,14 +1,5 @@
 import React, {useEffect, useState, useRef} from 'react';
-import {
-  View,
-  Text,
-  Button,
-  Alert,
-  StyleSheet,
-  Platform,
-  TouchableOpacity,
-  Image,
-} from 'react-native';
+import { View, Text, Button, Alert, StyleSheet, Platform, TouchableOpacity, Image, } from 'react-native';
 import {RNCamera} from 'react-native-camera';
 import {PERMISSIONS, request, RESULTS} from 'react-native-permissions';
 
@@ -47,18 +38,10 @@ const CameraUse: React.FC = () => {
   };
 
   if (hasPermission === null) {
-    return (
-      <View>
-        <Text>카메라 권한을 요청 중입니다...</Text>
-      </View>
-    );
+    return <View><Text>카메라 권한을 요청 중입니다...</Text></View>;
   }
   if (hasPermission === false) {
-    return (
-      <View>
-        <Text>카메라 접근 권한이 없습니다. 설정에서 권한을 허용해주세요.</Text>
-      </View>
-    );
+    return <View><Text>카메라 접근 권한이 없습니다. 설정에서 권한을 허용해주세요.</Text></View>;
   }
 
   return (
@@ -69,39 +52,23 @@ const CameraUse: React.FC = () => {
             source={{uri: `data:image/jpeg;base64,${photoData}`}}
             style={{flex: 1}}
           />
-          <TouchableOpacity
-            onPress={() => setPhotoData(null)}
-            style={styles.capture}>
-            <Text style={{fontSize: 14}}> 다시 찍기 </Text>
-          </TouchableOpacity>
+          <Button title="다시찍기" onPress={()=>setPhotoData(null)}/>
         </View>
       ) : (
+        <View style={{flex: 1}}>
         <RNCamera
           ref={cameraRef}
           style={{flex: 1}}
           type={RNCamera.Constants.Type.back}
           captureAudio={false}>
-          <View style={{flex: 1}}>
-            <TouchableOpacity onPress={takePicture} style={styles.capture}>
-              <Text style={{fontSize: 14}}> SNAP </Text>
-            </TouchableOpacity>
-          </View>
         </RNCamera>
+        
+        <Button title="SNAP" onPress={takePicture} />
+        </View>
       )}
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20,
-  },
-});
 
 export default CameraUse;
