4dbdbf40a50a7593c6e154c3c50fe1fc2152de3f
jaeyoung <choijy5847@naver.com>
2024-08-09 10:43:49 +0900

    fix: add routing to camera page

--- src/QrPage.tsx
+++ src/QrPage.tsx
@@ -1,8 +1,8 @@
-
+import { useNavigate } from 'react-router-dom';
 import QRCode from 'qrcode.react';
 
 function QrPage(){
-
+  const navigate = useNavigate();
   const cameraAppUrl = "http://127.0.0.1:5173/camera";
 
   return (
@@ -10,6 +10,7 @@ function QrPage(){
       <div style={{ textAlign: 'center', marginTop: '50px' }}>
         <h1>Scan this QR Code</h1>
         <QRCode value={cameraAppUrl} size={256} />
+        <button onClick={()=>{navigate('/camera');}}>Camera</button>
       </div>
     </>
   );
