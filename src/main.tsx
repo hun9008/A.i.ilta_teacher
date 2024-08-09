f5b2c3b3a653802d060c54f05131b85b37b21fb1
jaeyoung <choijy5847@naver.com>
2024-08-09 10:33:59 +0900

    build: fix camera & main code

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './App.tsx'
 import QrPage from './QrPage.tsx'
 import CameraPage from './camera.tsx';
 import './index.css'
