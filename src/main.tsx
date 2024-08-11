a904fd7afa985f06ca076f11b0138b258c683c4b
jerry <jerrylee3305@gmail.com>
2024-08-11 10:58:33 +0900

    Fix : SignUp Page Error

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,8 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/SignIn" element={<SignIn />} />
+
         <Route path="/QrPage" element={<QrPage />} />
         <Route path="/camera" element={<CameraPage />} />
         <Route path="/StudyGoals" element={<StudyGoals />} />
