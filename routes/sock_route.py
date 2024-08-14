43cf38000b8f1770ef8e8389b342d1187f542865
jyHun <younghune135@gmail.com>
2024-08-14 10:51:38 +0900

    CHORE : modify dictionary issue

--- routes/sock_route.py
+++ routes/sock_route.py
@@ -13,7 +13,7 @@ performing_ocr = False
 @route.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    connections.append(websocket)
+    # connections.append(websocket)
     try:
         while True:
             data = await websocket.receive_text()
