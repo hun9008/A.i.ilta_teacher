679c979566a515b81e01e8f6d6690d90f37a6025
jerry <jerrylee3305@gmail.com>
2024-08-15 18:50:56 +0900

    Fix : Change setting Page step and web camera socket code

--- src/SettingPage.tsx
+++ src/SettingPage.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { ChevronRight, Check } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import QrPage from './QrPage';
+import Camera from './camera.tsx';
 
 interface Step {
   id: string;
@@ -9,18 +10,18 @@ interface Step {
 }
 
 const steps: Step[] = [
+  { id: 'webcam', title: '웹캠 설정' },
   { id: 'qr', title: '모바일 연결' },
   { id: 'mobcam', title: '모바일 카메라 설정' },
-  { id: 'webcam', title: '웹캠 설정' },
   { id: 'goals', title: '공부 목표 설정' },
-  { id: 'complete', title: '설정 완료'}
+  { id: 'complete', title: '설정 완료' },
 ];
 
 function SettingPage() {
   const [currentStep, setCurrentStep] = useState(0);
   const [completed, setCompleted] = useState<Record<string, boolean>>({});
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const updateCompletedSteps = (stepIndex: number) => {
     const newCompleted: Record<string, boolean> = {};
@@ -53,13 +54,20 @@ function SettingPage() {
       case 'qr':
         return <QrPage />;
       case 'mobcam':
-        return <p>모바일 카메라 잘 뜨는지 확인 후 자바라 잘 설치하시오, 빛의 양을 조절하시오 등등</p>;
+        return (
+          <p>
+            모바일 카메라 잘 뜨는지 확인 후 자바라 잘 설치하시오, 빛의 양을
+            조절하시오 등등
+          </p>
+        );
       case 'webcam':
-        return <p>웹캠이 얼굴을 잘 찍고 있는지 확인</p>;
+        return <Camera />;
       case 'goals':
         return <p>공부 목표를 설정해주세요.</p>;
       case 'complete':
-        return <p>모든 설정이 완료되었습니다. 공부를 시작할 준비가 되었습니다.</p>;
+        return (
+          <p>모든 설정이 완료되었습니다. 공부를 시작할 준비가 되었습니다.</p>
+        );
       default:
         return null;
     }
@@ -72,16 +80,23 @@ function SettingPage() {
         <ol className="flex items-center">
           {steps.map((step, index) => (
             <li key={step.id} className="flex items-center">
-
-              <div className={`flex items-center justify-center w-7 h-7 rounded-full border-2 ${
-                index <= currentStep ? 'bg-primary-400 border-primary-400 text-white' : 'border-gray-300'
-              }`}>
+              <div
+                className={`flex items-center justify-center w-7 h-7 rounded-full border-2 ${
+                  index <= currentStep
+                    ? 'bg-primary-400 border-primary-400 text-white'
+                    : 'border-gray-300'
+                }`}
+              >
                 {completed[step.id] ? <Check size={16} /> : index + 1}
               </div>
 
-              <span className={`ml-2 text-sm ${
-                index <= currentStep ? 'text-primary-400 font-medium' : 'text-gray-500'
-              }`}>
+              <span
+                className={`ml-2 text-sm ${
+                  index <= currentStep
+                    ? 'text-primary-400 font-medium'
+                    : 'text-gray-500'
+                }`}
+              >
                 {step.title}
               </span>
               {index < steps.length - 1 && (
@@ -93,10 +108,12 @@ function SettingPage() {
       </div>
 
       <div className="bg-white shadow rounded-2xl p-6 mb-6">
-        <h2 className="text-xl font-semibold mb-4">{steps[currentStep].title}</h2>
+        <h2 className="text-xl font-semibold mb-4">
+          {steps[currentStep].title}
+        </h2>
         {renderStepContent(steps[currentStep])}
       </div>
-      
+
       <div className="flex justify-between">
         <button
           onClick={prevStep}
@@ -106,7 +123,11 @@ function SettingPage() {
           이전
         </button>
         <button
-          onClick={currentStep === steps.length -1 ? ()=> navigate('/StudyMain') : nextStep}
+          onClick={
+            currentStep === steps.length - 1
+              ? () => navigate('/StudyMain')
+              : nextStep
+          }
           className="px-4 py-2 bg-primary-400 text-white rounded-2xl hover:bg-primary-500"
         >
           {currentStep === steps.length - 1 ? '완료' : '다음'}
--- src/camera.tsx
+++ src/camera.tsx
@@ -1,192 +1,30 @@
-// import { useEffect, useState, useRef } from 'react';
-// import LaptopImage from './assets/Laptop.jpg';
-// import { useNavigate } from 'react-router-dom';
-
-// function CameraPage() {
-//   const videoRef = useRef<HTMLVideoElement>(null);
-//   const [ws, setWs] = useState<WebSocket | null>(null);
-//   // const [ocrText, setOcrText] = useState<string>('');
-//   const [errorMessage, setErrorMessage] = useState<string>('');
-//   const navigate = useNavigate();
-//   const localStreamRef = useRef<MediaStream | null>(null);
-//   const captureIntervalRef = useRef<number | null>(null);
-
-//   useEffect(() => {
-//     const constraints = {
-//       video: {
-//         facingMode: 'user',
-//       },
-//     };
-
-//     navigator.mediaDevices
-//       .getUserMedia(constraints)
-//       .then((stream) => {
-//         if (videoRef.current) {
-//           videoRef.current.srcObject = stream;
-//           localStreamRef.current = stream;
-//         }
-//       })
-//       .catch((err) => {
-//         console.error('Error accessing camera: ', err);
-//         setErrorMessage('Error accessing camera. Check console for details.');
-//       });
-
-//     return () => {
-//       stopStreaming();
-//     };
-//   }, []);
-
-//   const initWebSocket = () => {
-//     const socket = new WebSocket(import.meta.env.VITE_SOCKET_URL);
-
-//     socket.onopen = () => {
-//       console.log('WebSocket connection opened');
-//       setErrorMessage('');
-//       setWs(socket); // WebSocket 객체를 setWs로 설정한 후에 캡처를 시작
-//       startCapturingFrames(); // WebSocket이 open된 이후에만 캡처를 시작
-//     };
-
-//     socket.onerror = (error) => {
-//       console.error('WebSocket error:', error);
-//       setErrorMessage('WebSocket error occurred. Check console for details.');
-//     };
-
-//     socket.onclose = () => {
-//       console.log('WebSocket connection closed');
-//       stopCapturingFrames();
-//     };
-//     console.log('WebSocket initialized:', socket); // 로그를 추가하여 WebSocket 객체를 확인
-//   };
-//   const startCapturingFrames = () => {
-//     if (captureIntervalRef.current) return;
-
-//     captureIntervalRef.current = window.setInterval(() => {
-//       captureFrame();
-//     }, 1000);
-//   };
-
-//   const stopCapturingFrames = () => {
-//     if (captureIntervalRef.current) {
-//       clearInterval(captureIntervalRef.current);
-//       captureIntervalRef.current = null;
-//     }
-//   };
-//   const stopStreaming = () => {
-//     if (ws) {
-//       ws.close();
-//       setWs(null);
-//     }
-//     if (localStreamRef.current) {
-//       localStreamRef.current.getTracks().forEach((track) => track.stop());
-//       localStreamRef.current = null;
-//     }
-//     if (videoRef.current) {
-//       videoRef.current.srcObject = null;
-//     }
-//     // setOcrText('');
-//     setErrorMessage('');
-//   };
-
-//   const resetStreaming = () => {
-//     stopStreaming();
-//     initWebSocket();
-//   };
-
-//   const captureFrame = async () => {
-//     console.log('captureFrame called');
-//     if (!videoRef.current) {
-//       console.log('Video element not available');
-//     }
-//     if (!ws) {
-//       console.log('WebSocket is null');
-//     } else {
-//       console.log('WebSocket readyState:', ws.readyState); // 현재 WebSocket 상태 로그
-//     }
-//     if (videoRef.current && ws && ws.readyState === WebSocket.OPEN) {
-//       const canvas = document.createElement('canvas');
-//       canvas.width = videoRef.current.videoWidth;
-//       canvas.height = videoRef.current.videoHeight;
-//       const ctx = canvas.getContext('2d');
-//       if (ctx) {
-//         ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-//         const frame = canvas.toDataURL('image/jpeg').split(',')[1];
-//         ws.send(JSON.stringify({ type: 'video-frame', payload: frame }));
-//         console.log('captureFrame send');
-//       }
-//     } else {
-//       setErrorMessage('WebSocket connection is not open.');
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <div>
-//         <img src={LaptopImage} style={{ width: '100px', height: '100px' }} />
-
-//         <video
-//           ref={videoRef}
-//           autoPlay
-//           playsInline
-//           style={{ width: '300px', height: '300px' }}
-//         ></video>
-
-//         <div className="button-container">
-//           <button id="startButton" className="button" onClick={initWebSocket}>
-//             Run
-//           </button>
-//           <button id="stopButton" className="button" onClick={stopStreaming}>
-//             Stop
-//           </button>
-//           <button id="resetButton" className="button" onClick={resetStreaming}>
-//             Reset
-//           </button>
-//           {/* <button id="sendButton" className="button" onClick={captureFrame}>
-//             Send
-//           </button> */}
-//         </div>
-//         {/* <div id="ocr-result">
-//             <h2>OCR Result:</h2>
-//             <p>{ocrText}</p>
-//         </div> */}
-//         <div>{errorMessage}</div>
-//       </div>
-//       <button onClick={() => navigate('/studygoals')}>학습하기</button>
-//     </div>
-//   );
-// }
-
-// export default CameraPage;
-
-/// <reference types="node" />
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import LaptopImage from './assets/Laptop.jpg';
-import { useNavigate } from 'react-router-dom';
 
 function CameraPage() {
   const videoRef = useRef<HTMLVideoElement>(null);
-  const [ws, setWs] = useState<WebSocket | null>(null);
-  const [errorMessage, setErrorMessage] = useState<string>('');
-  const navigate = useNavigate();
+  const wsRef = useRef<WebSocket | null>(null);
   const localStreamRef = useRef<MediaStream | null>(null);
-  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const captureIntervalRef = useRef<number | null>(null);
+  const u_id = localStorage.getItem('u_id');
+  const reconnectAttemptsRef = useRef<number>(0);
 
   useEffect(() => {
     const constraints = {
-      video: {
-        facingMode: 'user',
-      },
+      video: { facingMode: 'user' },
     };
+
     navigator.mediaDevices
       .getUserMedia(constraints)
       .then((stream) => {
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
           localStreamRef.current = stream;
+          console.log('Camera stream started:', stream);
         }
       })
       .catch((err) => {
         console.error('Error accessing camera: ', err);
-        setErrorMessage('Error accessing camera. Check console for details.');
       });
 
     return () => {
@@ -195,99 +33,151 @@ function CameraPage() {
   }, []);
 
   const initWebSocket = () => {
-    const socket = new WebSocket(import.meta.env.VITE_SOCKET_URL);
+    if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
+      console.log('WebSocket is already open');
+      return;
+    }
 
-    socket.onopen = () => {
+    console.log('Initializing WebSocket...');
+    wsRef.current = new WebSocket(import.meta.env.VITE_SOCKET_URL);
+
+    wsRef.current.onopen = () => {
       console.log('WebSocket connection opened');
-      setErrorMessage('');
-      startAutoCapture(); // Start sending frames automatically
+      reconnectAttemptsRef.current = 0; // Reset reconnect attempts on successful connection
+      startCapturingFrames();
     };
 
-    socket.onerror = (error) => {
+    wsRef.current.onerror = (error) => {
       console.error('WebSocket error:', error);
-      setErrorMessage('WebSocket error occurred. Check console for details.');
     };
 
-    socket.onclose = () => {
+    wsRef.current.onmessage = (event) => {
+      console.log('WebSocket message received:', event.data);
+    };
+
+    wsRef.current.onclose = (event) => {
       console.log('WebSocket connection closed');
-      stopAutoCapture(); // Stop sending frames when the WebSocket is closed
+      console.log('Close event:', event);
+      stopCapturingFrames();
+      if (event.code !== 1000) {
+        // 1000 is a normal closure, attempt to reconnect if not
+        attemptReconnect();
+      }
     };
 
-    setWs(socket);
+    console.log('WebSocket initialized:', wsRef.current);
+  };
+
+  const attemptReconnect = () => {
+    const maxReconnectAttempts = 5;
+    if (reconnectAttemptsRef.current < maxReconnectAttempts) {
+      reconnectAttemptsRef.current += 1;
+      console.log(
+        `Attempting to reconnect... (${reconnectAttemptsRef.current}/${maxReconnectAttempts})`
+      );
+      setTimeout(initWebSocket, 1000 / 20); // Try to reconnect after 1 second
+    } else {
+      console.log('Max reconnect attempts reached.');
+    }
+  };
+
+  const startCapturingFrames = () => {
+    if (captureIntervalRef.current) return;
+
+    console.log('Starting to capture frames...');
+    captureIntervalRef.current = window.setInterval(() => {
+      captureFrame();
+    }, 1000);
+  };
+
+  const stopCapturingFrames = () => {
+    if (captureIntervalRef.current) {
+      console.log('Stopping frame capture...');
+      clearInterval(captureIntervalRef.current);
+      captureIntervalRef.current = null;
+    }
   };
 
   const stopStreaming = () => {
-    stopAutoCapture(); // Stop sending frames when streaming stops
-
-    if (ws) {
-      ws.close();
-      setWs(null);
-    }
     if (localStreamRef.current) {
+      console.log('Stopping camera stream...');
       localStreamRef.current.getTracks().forEach((track) => track.stop());
       localStreamRef.current = null;
     }
     if (videoRef.current) {
+      console.log('Releasing video element...');
       videoRef.current.srcObject = null;
     }
-    setErrorMessage('');
+    stopCapturingFrames();
   };
 
-  const startAutoCapture = () => {
-    if (intervalRef.current) return; // Prevent multiple intervals
-
-    intervalRef.current = setInterval(() => {
-      captureFrame();
-    }, 1000); // Capture a frame every second
-  };
-
-  const stopAutoCapture = () => {
-    if (intervalRef.current) {
-      clearInterval(intervalRef.current);
-      intervalRef.current = null;
+  const captureFrame = () => {
+    console.log('captureFrame called');
+    if (!videoRef.current) {
+      console.log('Video element not available');
+      return;
     }
-  };
+    if (!wsRef.current) {
+      console.log('WebSocket is null');
+      return;
+    }
 
-  const captureFrame = async () => {
-    if (videoRef.current && ws && ws.readyState === WebSocket.OPEN) {
-      const canvas = document.createElement('canvas');
-      canvas.width = videoRef.current.videoWidth;
-      canvas.height = videoRef.current.videoHeight;
-      const ctx = canvas.getContext('2d');
-      if (ctx) {
-        ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-        const frame = canvas.toDataURL('image/jpeg').split(',')[1];
-        ws.send(JSON.stringify({ type: 'video-frame', payload: frame }));
-      }
-    } else {
-      setErrorMessage('WebSocket connection is not open.');
+    if (wsRef.current.readyState !== WebSocket.OPEN) {
+      console.log('WebSocket is not open');
+      return;
+    }
+
+    const canvas = document.createElement('canvas');
+    canvas.width = videoRef.current.videoWidth;
+    canvas.height = videoRef.current.videoHeight;
+    const ctx = canvas.getContext('2d');
+    if (ctx) {
+      ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+      const frame = canvas.toDataURL('image/jpeg').split(',')[1];
+      const message = JSON.stringify({
+        type: 'video',
+        payload: frame,
+        device: 'pc',
+        u_id: u_id,
+      });
+      wsRef.current.send(message);
+      console.log('captureFrame sent');
     }
   };
 
   return (
-    <div>
-      <div>
-        <img src={LaptopImage} style={{ width: '100px', height: '100px' }} />
-
+    <div className="flex flex-col items-center justify-center bg-gray-50 min-h-screen">
+      <h1 className="text-xl font-bold mb-6 mt-6 text-center">
+        집중도를 확인 하기 위해 카메라 세팅이 필요해요.
+        <br />
+        사진과 같이 카메라를 조정해주세요.
+      </h1>
+      <div className="flex flex-row items-center justify-center space-x-8">
+        <div className="text-center">
+          <img
+            src={LaptopImage}
+            className="w-72 h-72 mb-10 p-4 bg-white rounded-xl animate-border-glow"
+          />
+        </div>
         <video
           ref={videoRef}
           autoPlay
           playsInline
-          style={{ width: '300px', height: '300px' }}
+          className="w-72 h-72 mb-10 p-4 bg-white rounded-xl animate-border-glow"
         ></video>
-
-        <div className="button-container">
-          <button id="startButton" className="button" onClick={initWebSocket}>
-            Run
-          </button>
-          <button id="stopButton" className="button" onClick={stopStreaming}>
-            Stop
-          </button>
-        </div>
-        <div>{errorMessage}</div>
       </div>
-      <button onClick={() => navigate('/StudyGoals')}>학습하기</button>
+
+      <div className="flex space-x-4">
+        <button
+          id="startButton"
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          onClick={initWebSocket}
+        >
+          Run
+        </button>
+      </div>
     </div>
   );
 }
+
 export default CameraPage;
