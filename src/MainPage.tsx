449c5704f6d80ec21f3b04af2b23e913966ace1a
jaeyoung <choijy5847@naver.com>
2024-08-23 19:41:04 +0900

    style: rerefine side banner

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -11,8 +11,8 @@ const MainPage: React.FC = () => {
   const isActive = (path: string) => location.pathname === path;
 
   return (
-    <div className="flex flex-row bg-primary-200 ">
-      <div className="flex flex-col items-center pt-[20px] bg-primary-100 w-16 h-screen fixed left-0 top-0">
+    <div className="flex flex-row h-screen bg-primary-200">
+      <div className="flex flex-col items-center pt-[30px] w-16 h-screen fixed left-0 top-0 ">
         <button
           className={`text-gray-400 mb-0 w-16 h-20 rounded-r-none py-2 ${isActive('/main') ? 'bg-white' : ''}`}
           onClick={() => navigate('/main')}
@@ -20,19 +20,19 @@ const MainPage: React.FC = () => {
           <img src={logo} alt="Logo" className="w-8 h-8 mx-auto" />
         </button>
         <button
-          className={`text-gray-400 mb-0 w-16 h-20 rounded-r-none py-2 ${isActive('/main/report') ? 'bg-white' : ''}`}
+          className={` mb-0 w-16 h-20 rounded-r-none py-2 ${isActive('/main/report') ? 'bg-white text-gray-500' : 'text-white'}`}
           onClick={() => navigate('/main/report')}
         >
           <FileText size={30} className="mx-auto" />
         </button>
         <button
-          className={`text-gray-400 mb-8 w-16 h-20 rounded-r-none py-2 ${isActive('/main/competition') ? 'bg-white' : ''}`}
+          className={`mb-8 w-16 h-20 rounded-r-none py-2 ${isActive('/main/competition') ? 'bg-white text-gray-500' : 'text-white'}`}
           onClick={() => navigate('/main/competition')}
         >
           <Trophy size={30} className="mx-auto" />
         </button>
       </div>
-      <div className="flex bg-white rounded-3xl flex-grow ">
+      <div className="flex bg-white rounded-xl flex-grow min-h-screen ml-16">
         <Outlet />
       </div>
     </div>
