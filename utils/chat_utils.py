02a84192798377a3bf5f94508569a32befb505dc
jyHun <younghune135@gmail.com>
2024-08-26 19:52:23 +0900

    CHORE: change util

--- utils/chat_utils.py
+++ utils/chat_utils.py
@@ -5,6 +5,9 @@ def prompt_delay(ocr, concept, user_text, prev_chat):
         prompt += "학생의 질문: " + user_text + "\n"
     else: 
         prompt = "아래 상황에서 나의 다음 답변을 두세 문장 안으로 말해줘. 단, 답변만 말해야 해.\n"
+        if type(ocr) == list:
+            # serialize list to string
+            ocr = '\n'.join(ocr)
         prompt += "나는 수학 선생님, 상대는 학생이야. 문제는 다음과 같아.\n" + ocr + "\n"
         prompt += "여기에 사용된 개념은 다음과 같아.\n" + concept + "\n"
         prompt += "학생의 질문: " + user_text + "\n"
@@ -17,6 +20,9 @@ def prompt_wrong(ocr, solution, user_text, prev_chat):
         prompt += "학생의 질문: " + user_text + "\n"
     else: 
         prompt = "아래 상황에서 나의 다음 답변을 말해줘. 단, 답변만 말해야 해.\n"
+        if type(ocr) == list:
+            # serialize list to string
+            ocr = '\n'.join(ocr)
         prompt += "나는 수학 선생님, 상대는 학생이야. 문제는 다음과 같아.\n" + ocr + "\n"
         prompt += "이 문제의 전체 풀이는 다음과 같아.\n" + solution + "\n"
         prompt += "단, 한 단계에 해당하는 답변만 간략하게 해줘.\n"
