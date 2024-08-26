cbb39aae2388f484fc34678a88eff9dd21c388cf
jyHun <younghune135@gmail.com>
2024-08-26 20:03:12 +0900

    fuck

--- utils/chat_utils.py
+++ utils/chat_utils.py
@@ -9,7 +9,11 @@ def prompt_delay(ocr, concept, user_text, prev_chat):
             # serialize list to string
             ocr = '\n'.join(ocr)
         prompt += "나는 수학 선생님, 상대는 학생이야. 문제는 다음과 같아.\n" + ocr + "\n"
+        if type(concept) == list:
+            concept = '\n'.join(concept)
         prompt += "여기에 사용된 개념은 다음과 같아.\n" + concept + "\n"
+        if type(user_text) == list:
+            user_text = '\n'.join(user_text)
         prompt += "학생의 질문: " + user_text + "\n"
         
     return prompt
@@ -24,8 +28,12 @@ def prompt_wrong(ocr, solution, user_text, prev_chat):
             # serialize list to string
             ocr = '\n'.join(ocr)
         prompt += "나는 수학 선생님, 상대는 학생이야. 문제는 다음과 같아.\n" + ocr + "\n"
+        if type(solution) == list:
+            solution = '\n'.join(solution)
         prompt += "이 문제의 전체 풀이는 다음과 같아.\n" + solution + "\n"
         prompt += "단, 한 단계에 해당하는 답변만 간략하게 해줘.\n"
+        if type(user_text) == list:
+            user_text = '\n'.join(user_text)
         prompt += "학생의 질문: " + user_text + "\n"
         
     return prompt
