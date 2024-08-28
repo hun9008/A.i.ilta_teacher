1275f530951c19627828d4f04f0665c5c5c4c469
Hanssamho <hanseungho@hanseunghoui-noteubug.local>
2024-08-28 18:38:17 +0900

    FIX: back doing status

--- utils/chat_utils.py
+++ utils/chat_utils.py
@@ -37,3 +37,22 @@ def prompt_wrong(ocr, solution, user_text, prev_chat):
         prompt += "학생의 질문: " + user_text + "\n"
         
     return prompt
+
+def prompt_doing(ocr, solution, user_text, prev_chat):
+    if prev_chat : # 이전 대화가 존재하는 경우 
+        prompt = prev_chat # 이전 대화 불러오기
+        prompt += "학생의 질문: " + user_text + "\n"
+    else: 
+        prompt = "아래 상황에서 나의 다음 답변을 말해줘. 단, 답변만 말해야 해.\n"
+        if type(ocr) == list:
+            # serialize list to string
+            ocr = '\n'.join(ocr)
+        prompt += "나는 수학 선생님, 상대는 학생이야. 문제는 다음과 같아. 최대한 간단히 대답해줘. \n" + ocr + "\n"
+        if type(solution) == list:
+            solution = '\n'.join(solution)
+        prompt += "이 문제의 전체 풀이는 다음과 같아.\n" + solution + "\n"
+        if type(user_text) == list:
+            user_text = '\n'.join(user_text)
+        prompt += "학생의 질문: " + user_text + "\n"
+        
+    return prompt
